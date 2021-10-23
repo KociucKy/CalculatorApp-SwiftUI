1da3fd1d2ba2bd883234e4fc22b2cb2aba0e1f6d
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-10-23 10:53:09 +0200

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Kuba Kociucki on 23/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
