a48c7e2186e3b4aa51b4213bf9db1131ed5ef374
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-10-23 11:21:22 +0200

    Fixed the CalculatorHome layout using GeometryReader

--- CalculatorApp/Views/CalculatorHome.swift
+++ CalculatorApp/Views/CalculatorHome.swift
@@ -14,26 +14,29 @@ let darkGray = Color(CGColor(gray: 0.3, alpha: 1))
 
 struct CalculatorHome: View {
     var body: some View {
-        VStack(alignment: .trailing, spacing: 0){
-            Spacer()
-            
-            Text("0")
-                .foregroundColor(.white)
-                .font(.system(size: 40))
-                .lineLimit(1)
-                .padding(.horizontal, 20)
-            
-            VStack(spacing: 10){
-                CalculatorRow(labels: ["CE", "", "", String("\u{00f7}")], colors: [darkGray, darkGray, darkGray, .orange])
-                CalculatorRow(labels: ["7", "8", "9", String("\u{00d7}")])
-                CalculatorRow(labels: ["4", "5", "6", "-"])
-                CalculatorRow(labels: ["1", "2", "3", "+"])
-                CalculatorRow(labels: ["0", ".", "", "="])
+        GeometryReader { geo in
+            VStack(alignment: .trailing, spacing: 0){
+                Spacer()
+                
+                Text("0")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40))
+                    .lineLimit(1)
+                    .padding(.horizontal, 20)
+                
+                VStack(spacing: 10){
+                    CalculatorRow(labels: ["CE", "", "", String("\u{00f7}")], colors: [darkGray, darkGray, darkGray, .orange])
+                    CalculatorRow(labels: ["7", "8", "9", String("\u{00d7}")])
+                    CalculatorRow(labels: ["4", "5", "6", "-"])
+                    CalculatorRow(labels: ["1", "2", "3", "+"])
+                    CalculatorRow(labels: ["0", ".", "", "="])
+                }
+                .frame(height: geo.size.height * 0.8)
+                .padding()
             }
-            .padding()
-        }
-        .background(darkerGray)
+            .background(darkerGray)
         .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
