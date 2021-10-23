34b9d58102b8692d14baf618484fd691e3899df4
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-10-23 11:04:13 +0200

    Create a calculator button component

---
+++ CalculatorApp/Views/CalculatorButton.swift
@@ -0,0 +1,37 @@
+//
+//  CalculatorButton.swift
+//  CalculatorApp
+//
+//  Created by Kuba Kociucki on 23/10/2021.
+//
+
+import SwiftUI
+
+struct CalculatorButton: View {
+    
+    var label: String
+    var color: Color
+    
+    var body: some View {
+        Button {
+            // Inform model of button pressed
+        } label: {
+            Circle()
+                .fill(color)
+                .frame(width: 100, height: 100)
+                .overlay(
+                    Text(label)
+                        .font(.title)
+                )
+        }
+        .accentColor(.white)
+
+    }
+}
+
+struct CalculatorButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorButton(label: "1", color: .gray)
+            .previewLayout(.fixed(width: 100, height: 100))
+    }
+}
